6536abf32cdac711b1fa8d863edcf0dd66fad327
ricamaral01 <ricardo.rda@gmail.com>
2026-04-10 07:16:54 -0300

    Layout Cartas Traco no Controle Estatistico + CNAME dominio personalizado

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
   Cacheia Home + todos os sub-apps
   ============================================= */
 
-const CACHE = "ct-hub-v29";
+const CACHE = "ct-hub-v30";
 
 const ASSETS = [
   /* ---- Splash ---- */
