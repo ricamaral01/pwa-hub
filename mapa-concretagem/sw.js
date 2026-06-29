c8ed9dd74b2a3669c270019e82bfe953d257f8be
ricamaral01 <ricardo.rda@gmail.com>
2026-06-29 16:55:13 -0300

    Atualiza cache do PWA

--- mapa-concretagem/sw.js
+++ mapa-concretagem/sw.js
@@ -2,7 +2,7 @@
    MAPA DE CONCRETAGEM — Service Worker Independente
    ========================================================= */
 
-const CACHE_NAME = "mapa-concretagem-v4.36";
+const CACHE_NAME = "mapa-concretagem-v4.37";
 const ASSETS = [
   "./",
   "./index.html",
