0e8c8a2f02d71f556df26b7110df67b8a69e89af
ricamaral01 <ricardo.rda@gmail.com>
2026-07-16 15:25:07 -0300

    fix: adicionar white-space nowrap nos tds e ths da tabela mandril para evitar quebras em tablets

--- mapa-concretagem/sw.js
+++ mapa-concretagem/sw.js
@@ -2,7 +2,7 @@
    MAPA DE CONCRETAGEM — Service Worker Independente
    ========================================================= */
 
-const CACHE_NAME = "mapa-concretagem-v4.68";
+const CACHE_NAME = "mapa-concretagem-v4.69";
 const ASSETS = [
   "./",
   "./index.html",
