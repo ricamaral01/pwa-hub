135c6a7c89dff683234726dd6490531fc9b3fe18
ricamaral01 <37339466+ricamaral01@users.noreply.github.com>
2026-08-13 14:12:39 -0300

    fix: bump dashboard defeitos sw to v125

--- mapa-concretagem-teste/app.js
+++ mapa-concretagem-teste/app.js
@@ -10051,7 +10051,7 @@ function init() {
     navigator.serviceWorker.addEventListener("message", (event) => {
       if (event.data?.type === "SW_RESET_DONE" && !refreshing) {
         refreshing = true;
-        window.location.replace(window.location.pathname + "?cache-reset=v124-dashboard-defeitos");
+        window.location.replace(window.location.pathname + "?cache-reset=v125-dashboard-defeitos");
       }
     });
     navigator.serviceWorker.addEventListener("controllerchange", () => {
@@ -10061,7 +10061,7 @@ function init() {
       }
     });
 
-    navigator.serviceWorker.register("./sw.js?v=1.24-dashboard-defeitos-reset").then((reg) => {
+    navigator.serviceWorker.register("./sw.js?v=1.25-dashboard-defeitos-reset").then((reg) => {
       reg.update().catch(() => {});
     }).catch(() => {});
   }
