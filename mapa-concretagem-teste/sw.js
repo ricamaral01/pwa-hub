e96670d8561ce5345f4b1b5b45274f729b39e415
ricamaral01 <ricardo.rda@gmail.com>
2026-08-18 12:19:45 -0300

    chore: forcar atualizacao sw mapa teste v136

--- mapa-concretagem-teste/sw.js
+++ mapa-concretagem-teste/sw.js
@@ -3,7 +3,7 @@
    v1.33-dashboard-defeitos: remove caches antigos e desativa o SW no ambiente teste
    ========================================================= */
 
-const CACHE_NAME = "mapa-concretagem-teste-v1.35";
+const CACHE_NAME = "mapa-concretagem-teste-v1.36";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(self.skipWaiting());
